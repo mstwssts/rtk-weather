c1b8cc867a01548891010ccfa91e28d77121a2df
Marvin Sierra <msierra6920@gmail.com>
2025-09-13 20:47:19 -0400

    store and provider
    I made the store and the provider, no reducer yet

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,17 @@
 import './globals.css'
+import { Provider } from 'react-redux'
+import { store } from './store/configureStore'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Provider store={store}>
+
+          {children}
+          
+        </Provider>
+      </body>
     </html>
   )
 }
---
+++ app/store/configureStore.js
@@ -0,0 +1,10 @@
+'use client'
+
+import { configureStore } from "@reduxjs/toolkit"
+
+//right now I have no reducers but thats ok for now
+export const store = configureStore({
+  reducer: {
+    //future reducer
+  },
+})
