a2dffe1dfe7e3246d57fe2ee6ddae27c26d51322
Marvin Sierra <msierra6920@gmail.com>
2025-09-17 11:10:49 -0400

    updated openWeather.js
    we added the logic for the API call using axios and createAsyncThunk as well as adding extraReducers to update slice

--- app/store/slices/weatherSlice.js
+++ app/store/slices/weatherSlice.js
@@ -1,6 +1,23 @@
 'use client'
 
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice,createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit"
+import { fetchForecast, forecastData } from "@/app/lib/openWeather"
+
+// createAsyncThunk handles the action for side effects  
+
+export const fetchCityForecast = createAsyncThunk(
+  async (city, { rejectedWithValue } ) => {
+    try {
+      const raw = await fetchForecast ( city )
+      return forecastData( raw )
+    }
+    catch ( err ){
+      const apiMsg = err?.response?.data?.message
+      if ( apiMsg ) return rejectedWithValue( apiMsg ) 
+        return rejectedWithValue( 'Make Believe City, Try Again')
+    }
+  }
+)
 
 const initialState = {
   status: 'idle',  //idle -> loading - success or fail
@@ -13,8 +30,6 @@ const initialState = {
   }
 }
 
-// A slice has name, initial state, and reducers
-
 const weatherSlice = createSlice({
   name: 'weather',
   initialState,
@@ -22,6 +37,26 @@ const weatherSlice = createSlice({
     clearError(state) {
       state.error = null
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCityForecast.pending, (state) => {
+        state.status = 'loading';
+        state.error = null; 
+      })
+
+      .addCase(fetchCityForecast.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.city = action.payload.city;
+        state.series.temps = action.payload.temps;
+        state.series.pressures = action.payload.pressures;
+        state.series.humidities = action.payload.humidities;
+      })
+
+      .addCase(fetchForecastByCity.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload || 'Error';
+      });
   }
 })
 
